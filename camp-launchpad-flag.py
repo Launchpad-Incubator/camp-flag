22549a398646ba051cb56ef02fc32db5028a9b4c
arduinohatesme <awmcmillan128@gmail.com>
2025-07-24 14:01:44 -0500

    Updated url

--- camp-launchpad-flag.py
+++ camp-launchpad-flag.py
@@ -11,18 +11,21 @@ Author: Aidan McMillan
 import keyboard
 import socket
 import requests
+import time
+
 
 # INSERT IP INFO HERE
 PICO_IP = "10.10.20.82"
 
 def send_command(command: str) -> None:
+    url = f"http://{PICO_IP}/api?{command}"
     try:
         response = requests.get(url, timeout=10)
         print(f"Sent: {command} - Status: {response.status_code}")
     except Exception as e:
         print(f"Failed to send: {command} - Error: {e}")
     
-def main()
+def main():
     print("Initialized flag control. Press SPACE to move flag.")
     last_cmd = None
 
