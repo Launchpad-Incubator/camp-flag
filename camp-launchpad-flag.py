5712fc899769bcbdedefa0bd8238ef74b8b8256f
arduinohatesme <awmcmillan128@gmail.com>
2025-07-24 13:12:52 -0500

    Init commit

---
+++ camp-launchpad-flag.py
@@ -0,0 +1,44 @@
+"""
+FLAG CODE FOR CAMP LAUNCHPAD V0
+
+- Connects to Raspberry Pi Pico W to control a servo motor.
+
+Update notes: Init commit
+
+Author: Aidan McMillan
+"""
+
+import keyboard
+import socket
+import requests
+
+# INSERT IP INFO HERE
+PICO_IP = "10.10.20.82"
+
+def send_command(command: str) -> None:
+    try:
+        response = requests.get(url, timeout=10)
+        print(f"Sent: {command} - Status: {response.status_code}")
+    except Exception as e:
+        print(f"Failed to send: {command} - Error: {e}")
+    
+def main()
+    print("Initialized flag control. Press SPACE to move flag.")
+    last_cmd = None
+
+    while True:
+        if keyboard.is_pressed('space'):
+            move_cmd = "move"
+
+        else:
+            move_cmd = "hold"
+
+        command = f"cmd={move_cmd}"
+
+        if command != last_cmd:
+            send_command(command)
+
+        time.sleep(0.02)
+
+main()
+        
